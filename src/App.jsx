3eee14baa17150d11782c4fe53e3c7ff3b436768
lz-coder <lzcoder@proton.me>
2024-09-17 03:59:49 -0300

    feat: use Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-      <h1>YATMAN</h1> 
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

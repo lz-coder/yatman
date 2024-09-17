22edf914b78006fb789243462ee5ed1a7eca2991
lz-coder <lzcoder@proton.me>
2024-09-17 03:58:35 -0300

    feat: implement initial Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { BiSearch } from "react-icons/bi";
+import IconButton from "./widgets/IconButton";
+
+const Header = () => {
+  return (
+    <header className="flex min-h-8 flex-col items-center justify-center bg-slate-700 py-2 font-bold text-white sm:flex-row">
+      <h1 className="mb-2 border-[1px] border-solid border-white px-2 sm:mb-0 sm:mr-2">
+        YATMAN
+      </h1>
+      <div className="flex items-center gap-2">
+        <input
+          type="search"
+          className="border-[1px] border-white bg-slate-500 px-1 text-black focus:bg-white"
+        ></input>
+        <IconButton
+          icon={<BiSearch size={22} />}
+          className="p-1 font-bold hover:bg-slate-400 hover:text-slate-700"
+        ></IconButton>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

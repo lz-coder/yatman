0f6c9234cea6f2e4605bd109641a5a715fbf33ec
lz-coder <lzcoder@proton.me>
2024-09-18 16:20:00 -0300

    refactor: move item creation logic to a function

--- src/components/ProjectGroupCreationModal.jsx
+++ src/components/ProjectGroupCreationModal.jsx
@@ -8,33 +8,67 @@ export default function ProjectGroupCreationModal({
   title,
   closeHandler,
   creationHandler,
+  groupMode = false,
+  selectedItems,
 }) {
-  const [createType, setCreateType] = useState("project");
+  const [createType, setCreateType] = useState(groupMode ? "group" : "project");
   const [itemName, setItemName] = useState("");
+
+  console.log(groupMode);
 
   function handleCreationType(e) {
     setCreateType(e.target.value);
   }
+
+  function createItem() {
+    let itemToCreate;
+    if (createType === "project" && itemName.trim().length > 0) {
+      itemToCreate = new ProjectModel(
+        itemName,
+        "red",
+        "default",
+        new Date(),
+        null,
+      );
+    } else if (createType === "group" && itemName.trim().length > 0) {
+      itemToCreate = new GroupModel(
+        itemName,
+        "blue",
+        "default",
+        new Date(),
+        null,
+      );
+      if (groupMode && selectedItems) itemToCreate.items = selectedItems;
+    }
+
+    if (itemToCreate) {
+      creationHandler(itemToCreate, groupMode);
+      closeHandler();
+    }
+  }
+
   return (
     <Modal title={title} closeHandler={closeHandler}>
       <div>
-        <div className="flex items-center justify-center gap-2">
-          <RadioOption
-            label="New Project"
-            radioName="itemType"
-            id="radio_project_option"
-            value="project"
-            onChange={handleCreationType}
-            selected={true}
-          />
-          <RadioOption
-            label="New Group"
-            radioName="itemType"
-            id="radio_group_option"
-            value="group"
-            onChange={handleCreationType}
-          />
-        </div>
+        {!groupMode && (
+          <div className="flex items-center justify-center gap-2">
+            <RadioOption
+              label="New Project"
+              radioName="itemType"
+              id="radio_project_option"
+              value="project"
+              onChange={handleCreationType}
+              selected={true}
+            />
+            <RadioOption
+              label="New Group"
+              radioName="itemType"
+              id="radio_group_option"
+              value="group"
+              onChange={handleCreationType}
+            />
+          </div>
+        )}
         <div className="my-4">
           <div className="flex flex-col">
             <label>{createType} name</label>
@@ -49,19 +83,7 @@ export default function ProjectGroupCreationModal({
         <div className="mt-8 text-center">
           <button
             className="bg-green-600 px-3 py-1 hover:bg-green-400"
-            onClick={() => {
-              let itemToCreate;
-              if (createType === "project" && itemName.trim().length > 0) {
-                itemToCreate = new ProjectModel(itemName, "red", "default");
-              } else if (createType === "group" && itemName.trim().length > 0) {
-                itemToCreate = new GroupModel(itemName, "blue", "default");
-              }
-
-              if (itemToCreate) {
-                creationHandler(itemToCreate);
-                closeHandler();
-              }
-            }}
+            onClick={() => createItem()}
           >
             Create {createType}
           </button>

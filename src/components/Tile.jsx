70f139703a51fd2199eff0f83ba354ea168a1fe5
lz-coder <lzcoder@proton.me>
2024-09-18 17:32:02 -0300

    feat: add drag n drop support

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,7 +2,7 @@ import ProjectModel from "../models/project";
 import GroupModel from "../models/group";
 import { FaBox, FaFolder } from "react-icons/fa";
 
-export default function Tile({ data, onCheckChange }) {
+export default function Tile({ data, onCheckChange, onDropped }) {
   let tileStyleClasses;
   // const tileStyleClasses = `bg-${data.background}-300 border-${data.background}-300 hover:border-${data.background}-400`;
   let isGroup = data instanceof GroupModel;
@@ -25,9 +25,44 @@ export default function Tile({ data, onCheckChange }) {
 
   const formattedDate = `${year}-${month}-${day}`;
 
+  const groupTileProps = isGroup
+    ? { onDrop: dropHandler, onDragOver: dragoverHandler }
+    : {};
+
+  function dragStartHandler(ev) {
+    const dataString = JSON.stringify(data);
+    ev.dataTransfer.setData("application/json", dataString);
+    ev.dataTransfer.dropEffect = "move";
+  }
+
+  function dragoverHandler(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  }
+  function dropHandler(ev) {
+    ev.preventDefault();
+    const dragData = ev.dataTransfer.getData("application/json");
+    const tileObject = JSON.parse(dragData);
+
+    const objectInstance = new ProjectModel(
+      tileObject.name,
+      tileObject.background,
+      tileObject.icon,
+      tileObject.creationDate,
+      tileObject.updatedDate,
+      tileObject.id,
+    );
+
+    data.items.push(objectInstance);
+    onDropped(objectInstance);
+  }
+
   return (
     <div
       className={`flex h-32 w-32 cursor-pointer flex-col justify-between border-2 border-solid p-1 ${tileStyleClasses}`}
+      draggable={isProject}
+      onDragStart={dragStartHandler}
+      {...groupTileProps}
     >
       <div className="flex h-1/5 justify-between">
         <input type="checkbox" onChange={(e) => onCheckChange(e, data)}></input>

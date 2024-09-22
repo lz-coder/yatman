d1e4a0e891cb21aef243e8a412b5c0ceb1621abc
lz-coder <lzcoder@proton.me>
2024-09-22 15:01:54 -0300

    feat: use InitialPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import "./App.css";
 import Header from "./components/Header";
+import InitialPage from "./pages/InitialPage";
 
 function App() {
   return (
     <>
       <Header />
+      <InitialPage />
     </>
   );
 }

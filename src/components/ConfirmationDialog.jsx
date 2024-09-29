f09f7a37f677fba273d0bc65f87058660ac8157b
lz-coder <lzcoder@proton.me>
2024-09-29 11:36:31 -0300

    feat: implement ConfirmationDialog component

---
+++ src/components/ConfirmationDialog.jsx
@@ -0,0 +1,47 @@
+import Modal from "./Modal";
+import IconLabelButton from "./widgets/IconLabelButton";
+
+const ConfirmationDialog = ({
+  title,
+  message,
+  isDestructive,
+  closeDialogCallback,
+  confirmationCallback,
+}) => {
+  const messageClasses = isDestructive ? "text-red-400" : "text-white";
+  const confirmButtonClasses = isDestructive
+    ? "bg-red-600 hover:bg-red-400"
+    : "bg-green-600 over:bg-green-400";
+  const buttonsClasses = "px-3 py-1";
+
+  return (
+    <Modal
+      title={title}
+      closeHandler={closeDialogCallback}
+      onKeyUp={closeDialogCallback}
+      titlebarClasses={`${isDestructive && "bg-orange-400"}`}
+    >
+      <div>
+        <p className={`text-center text-lg font-semibold ${messageClasses}`}>
+          {message}
+        </p>
+        <div className="mt-6 flex items-center justify-center gap-4">
+          <IconLabelButton
+            label="Delete"
+            showIcon={false}
+            onClick={confirmationCallback}
+            className={`${buttonsClasses} ${confirmButtonClasses}`}
+          />
+          <IconLabelButton
+            label="Cancel"
+            showIcon={false}
+            className={`${buttonsClasses} bg-blue-600 hover:bg-blue-400`}
+            onClick={closeDialogCallback}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmationDialog;

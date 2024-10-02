c1be80778e281687221ea9bf3cb2640723d446b2
lz-coder <lzcoder@proton.me>
2024-10-02 19:01:44 -0300

    refactor: formatter

--- src/models/group.js
+++ src/models/group.js
@@ -1,11 +1,18 @@
 import TileModel from "./tile";
 
 export default class GroupModel extends TileModel {
-  constructor(name, background, icon, creationDate, updatedDate, id = crypto.randomUUID()) {
+  constructor(
+    name,
+    background,
+    icon,
+    creationDate,
+    updatedDate,
+    id = crypto.randomUUID(),
+  ) {
     super(id, name, background, icon, creationDate, updatedDate);
     this.items = [];
   }
-  
+
   insertItem(item) {
     this.items.push(item);
   }
--- src/models/project.js
+++ src/models/project.js
@@ -1,7 +1,14 @@
 import TileModel from "./tile";
 
 export default class ProjectModel extends TileModel {
-  constructor(name, background, icon, creationDate, updatedDate, id = crypto.randomUUID()) {
+  constructor(
+    name,
+    background,
+    icon,
+    creationDate,
+    updatedDate,
+    id = crypto.randomUUID(),
+  ) {
     super(id, name, background, icon, creationDate, updatedDate);
   }
 }

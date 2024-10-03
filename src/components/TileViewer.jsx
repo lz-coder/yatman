d61817d5693eede9eb0b370bf59543380a052631
lz-coder <lzcoder@proton.me>
2024-10-03 14:49:21 -0300

    feat: use TileManager

--- src/components/TileViewer.jsx
+++ src/components/TileViewer.jsx
@@ -1,16 +1,10 @@
-import GroupModel from "../models/group";
 import Modal from "./Modal";
-import Tile from "./Tile";
+import TileManager from "./TileManager";
 
 export default function TileViewer({ tile, closeCallback }) {
   return (
     <Modal title={tile.name} closeHandler={closeCallback}>
-      <div className="w-full max-w-7xl">
-        {tile instanceof GroupModel &&
-          tile.items.map((item) => {
-            return <Tile key={item.id} data={item} onGroup={true} />;
-          })}
-      </div>
+      <TileManager tilesStorage={tile.items} onGroup={true} />
     </Modal>
   );
 }

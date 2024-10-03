5a138c3cebe5429421564f25493d57565c38473c
lz-coder <lzcoder@proton.me>
2024-10-03 14:46:49 -0300

    feat: better modal position and add dialogClasses prop

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,20 @@
 import { IoClose } from "react-icons/io5";
 
-const Modal = ({ title, children, closeHandler, onKeyUp, titlebarClasses }) => {
+const Modal = ({
+  title,
+  children,
+  closeHandler,
+  onKeyUp,
+  titlebarClasses,
+  dialogClasses,
+}) => {
   return (
     <div
-      className="fixed left-0 top-0 h-full w-full overflow-hidden bg-black bg-opacity-20"
+      className="fixed left-0 top-0 grid h-full w-full place-items-center overflow-hidden bg-black bg-opacity-20"
       onClick={closeHandler}
     >
       <div
-        className="mx-auto mt-36 flex max-w-xs flex-col border-2 border-solid border-slate-700 bg-slate-600 p-2 text-white md:max-w-xl"
+        className={`mx-auto mb-40 flex min-w-80 max-w-xs flex-col border-2 border-solid border-slate-700 bg-slate-600 p-2 text-white md:max-w-xl ${dialogClasses}`}
         onClick={(e) => e.stopPropagation()}
         onKeyUp={onKeyUp}
       >

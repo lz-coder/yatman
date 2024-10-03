8c751bc26921943111fedd976f95c809fd579399
lz-coder <lzcoder@proton.me>
2024-10-03 15:07:11 -0300

    style: add some dialogClasses

--- src/components/TileViewer.jsx
+++ src/components/TileViewer.jsx
@@ -3,7 +3,11 @@ import TileManager from "./TileManager";
 
 export default function TileViewer({ tile, closeCallback }) {
   return (
-    <Modal title={tile.name} closeHandler={closeCallback}>
+    <Modal
+      title={tile.name}
+      closeHandler={closeCallback}
+      dialogClasses="md:max-w-6xl md:!w-full h-4/6"
+    >
       <TileManager tilesStorage={tile.items} onGroup={true} />
     </Modal>
   );

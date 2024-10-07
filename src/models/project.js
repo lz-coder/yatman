79ee15ced0d1ea4ee9cc21e0a1e1bec855a87502
lz-coder <lzcoder@proton.me>
2024-10-07 11:20:31 -0300

    feat: add tasks support

--- src/models/project.js
+++ src/models/project.js
@@ -1,3 +1,4 @@
+import TaskModel from "./task";
 import TileModel from "./tile";
 
 export default class ProjectModel extends TileModel {
@@ -10,5 +11,22 @@ export default class ProjectModel extends TileModel {
     id = crypto.randomUUID(),
   ) {
     super(id, name, background, icon, creationDate, updatedDate);
+    this.tasks = [];
+  }
+
+  set setTasks(val) {
+    this.tasks = val;
+  }
+
+  addTask(task) {
+    if (!(task instanceof TaskModel))
+      throw new Error("The task to add need to be a TaskModel instance");
+    this.tasks.push(task);
+  }
+
+  removeTask(task) {
+    const taskInTasks = this.tasks.indexOf(task);
+    if (!taskInTasks) throw new Error("Given task not founded");
+    this.tasks.splice(taskInTasks, 1);
   }
 }

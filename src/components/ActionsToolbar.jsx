84e2d1ca13964269ab4294a6f4a80f1ba0a486fe
lz-coder <lzcoder@proton.me>
2024-10-07 11:10:54 -0300

    feat: add vAlignButtons used to control IconLabelButton vAlign prop

--- src/components/ActionsToolbar.jsx
+++ src/components/ActionsToolbar.jsx
@@ -1,6 +1,6 @@
 import IconLabelButton from "./widgets/IconLabelButton";
 
-const ActionsToolbar = ({ actions, className }) => {
+const ActionsToolbar = ({ actions, className, vAlignButtons = true }) => {
   const buttonStyleClasses =
     "hover:border-[2px] border-2 hover:border-slate-500 p-2 w-20 h-18";
 
@@ -14,7 +14,7 @@ const ActionsToolbar = ({ actions, className }) => {
               label={action.label}
               icon={action.icon}
               showLabel={true}
-              vAlign={true}
+              vAlign={vAlignButtons}
               className={buttonStyleClasses}
               onClick={action.action}
             />

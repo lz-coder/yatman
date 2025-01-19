bf65aad6110adcc1527dcf3b71359a7dc2949cec
lz-coder <lzcoder@proton.me>
2025-01-19 00:04:00 -0300

    feat: implement a basic working login/signup page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const activeTabStyles =
   "border-l border-r border-t border-gray-600 text-lg font-semibold";
@@ -7,28 +7,96 @@ const inactiveTabStyles = "border-b border-gray-600";
 
 export default function LoginPage() {
   const [newUser, setNewUser] = useState(false);
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  async function doLogin(e) {
-    console.log(username);
-    console.log(password);
+  const formRef = useRef();
+
+  async function submitForm(e) {
+    if (!e.target.checkValidity()) return;
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/login", {
-      method: "POST",
+
+    let url = "http://localhost:3000/login";
+
+    const formData = new FormData(formRef.current);
+
+    const reqBody = {
+      username: formData.get("username"),
+      password: formData.get("password"),
+    };
+
+    if (newUser) {
+      url = "http://localhost:3000/user";
+      reqBody.email = formData.get("email");
+    }
+
+    const response = await fetch(url, {
+      method: formRef.current.method,
       headers: { "Content-Type": "application/json" },
       credentials: "include",
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
+      body: JSON.stringify(reqBody),
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
+    const data = await response.json();
+    if (!response.ok) {
+      data.message && setErrorMessage(data.message);
+      return;
     }
+
+    console.log(data);
+  }
+
+  function genFormInputs(login) {
+    let buttonText = login ? "Log in" : "Sign up";
+
+    return (
+      <div className="flex flex-col">
+        {!login && (
+          <>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter a valid email here..."
+              id="email"
+              name="email"
+              required
+            />
+          </>
+        )}
+        <label htmlFor="username">Username</label>
+        <input
+          required
+          type="text"
+          placeholder="Enter your Username..."
+          id="username"
+          name="username"
+        />
+        <label htmlFor="password" className="mt-2">
+          Password
+        </label>
+        <input
+          required
+          minLength={6}
+          type="password"
+          placeholder="Enter your Password..."
+          id="password"
+          name="password"
+        />
+
+        {errorMessage && (
+          <p className="mt-10 text-center text-sm font-semibold text-red-700">
+            {errorMessage}
+          </p>
+        )}
+
+        <div className="absolute bottom-4 left-0 flex w-full justify-center">
+          <button>{buttonText}</button>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -44,51 +112,39 @@ export default function LoginPage() {
         <div className="max-h-96 w-4/5 md:h-4/6">
           <div className="flex justify-center">
             <button
-              onClick={() => setNewUser(false)}
+              onClick={() => {
+                setErrorMessage(null);
+                setNewUser(false);
+              }}
               className={`${tabStyles} ${!newUser ? activeTabStyles : inactiveTabStyles}`}
             >
               Login
             </button>
             <button
-              onClick={() => setNewUser(true)}
+              onClick={() => {
+                setErrorMessage(null);
+                setNewUser(true);
+              }}
               className={`${tabStyles} ${newUser ? activeTabStyles : inactiveTabStyles}`}
             >
               New Account
             </button>
           </div>
-          <div className="h-full border border-t-0 border-gray-600 px-4">
-            {newUser ? (
-              <form>
-                <p>Create your account!</p>
-                <p>Its free, easy and you will love the result</p>
-                {/* <input type="text" placeholder="Usename" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" /> */}
-              </form>
-            ) : (
-              <form className="flex flex-col">
-                <p>Enter in your account!</p>
-                <label htmlFor="username">Username</label>
-                <input
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  type="text"
-                  placeholder="Enter your Username..."
-                  id="username"
-                />
-                <label htmlFor="password" className="mt-2">
-                  Password
-                </label>
-                <input
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  placeholder="Enter your Password..."
-                  id="password"
-                />
-                <button onClick={doLogin}>Login</button>
-              </form>
-            )}
+          <div className="relative h-full border border-t-0 border-gray-600 px-4">
+            <form ref={formRef} onSubmit={submitForm} method="POST">
+              {newUser ? (
+                <>
+                  <p>Create your account!</p>
+                  <p>Its free, easy and you will love the result</p>
+                </>
+              ) : (
+                <>
+                  <p>Enter in your account!</p>
+                </>
+              )}
+
+              {genFormInputs(!newUser)}
+            </form>
           </div>
         </div>
       </div>
